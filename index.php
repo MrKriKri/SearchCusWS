8b423942e7c3c272c0802d5454ff677f6f062319
MrKriKri <stura.wasit@gmail.com>
2017-04-21 11:28:33 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,10 @@ $dbname = "AdventureWorks";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
 // if ($conn->connect_error) {
 //    die("Connection failed: " . $conn->connect_error);
 //} 
