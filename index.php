9d2ee9168b36465705a96291bb34a56a53f39c68
MrKriKri <stura.wasit@gmail.com>
2017-04-21 11:23:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php 
-$servername = "codelabdb.database.windows.net";
-$username = "codelab";
-$password = "abc123**";
-$dbname = "AdventureWorks";
+//$servername = "codelabdb.database.windows.net";
+//$username = "codelab";
+//$password = "abc123**";
+//$dbname = "AdventureWorks";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+//$conn = new mysqli($servername, $username, $password, $dbname);
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
+// if ($conn->connect_error) {
+//    die("Connection failed: " . $conn->connect_error);
+//} 
 
 echo "1";
 echo "hello";  ?>
