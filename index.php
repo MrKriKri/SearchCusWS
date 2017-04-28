512a1a10a714e4a5d074b8d70ba1e43d178e8491
MrKriKri <stura.wasit@gmail.com>
2017-04-28 10:47:58 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,12 +4,14 @@
 </head>
 <body>
 
-<form name="frmMain" method="post" action="">
+
 	  <h2>Search Customer</h2>
 	  Search by Country Code 
-	  <input type="text" name="txtCountry" value="<?php echo $_POST["txtCountry"];?>">
-	  <input type="submit" name="Submit" value="Submit">
-</form>
+	  <input type="text" name="txtCountry" value="" id = "Product">
+	  <input type="submit" name="Submit" value="Submit" id="Submit">
+	<div class ="" id = "result">
+	
+	</div>
 
 
 </body>
